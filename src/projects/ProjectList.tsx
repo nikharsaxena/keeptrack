3faa9f154332d986785c88c1c665b10f54561272
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-08-21 11:23:56 -0700

    Projects being displayed with forms

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,22 @@
+import type { Project } from './Project'
+import ProjectCard from './ProjectCard'
+import ProjectForm from './ProjectForm';
+
+interface ProjectListProps {
+    projects: Project[];
+}
+
+function ProjectList({ projects }: ProjectListProps) {
+    return (
+        <div className="row">
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    <ProjectCard project={project}/>
+                    <ProjectForm />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ProjectList;
