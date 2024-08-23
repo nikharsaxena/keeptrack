330e8080acc0777a8709bde1d7b8df345987052e
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-08-23 00:33:54 -0700

    Make project edit successful

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,16 +1,22 @@
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { useState } from "react";
 
 function ProjectsPage() {
+    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+
     const saveProject = (project: Project) => {
-        console.log('Saving prject: ', project);
+        let updatedProjects = projects.map((p: Project) => {
+            return p.id === project.id ? project : p
+        });
+        setProjects(updatedProjects);
     }
 
     return (
         <>
             <h1>Projects</h1>
-            <ProjectList onSave={saveProject} projects={MOCK_PROJECTS}/>
+            <ProjectList onSave={saveProject} projects={projects}/>
         </>
     );
 }
