38b41e3ccc2e29e75474e5aeca414afed1b15bf2
Nikhar Saxena <nikhar.saxena@sentry.io>
2024-08-26 11:03:54 -0700

    Handle separate Project Page with its own route

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import type { Project } from './Project';
+import {Link} from 'react-router-dom';
 
 function formatDescription(description: string): string {
     return `${description.substring(0, 60)}...`;
@@ -18,11 +19,13 @@ function ProjectCard({ project, onEdit }: ProjectCardProps) {
         <div className='card'>
             <img src={project.imageUrl} alt={project.name} />
             <section className='section dark'>
+                <Link to={'/projects/' + project.id}>
                 <h5 className='strong'>
                     <strong>{project.name}</strong>
                 </h5>
                 <p>{formatDescription(project.description)}</p>
                 <p>Budget : {project.budget.toLocaleString()}</p>
+                </Link>
                 <button 
                     className='bordered'
                     onClick={() => {
